3761c74c0481fac90461bf6a62aff730e4105ce5
dogra1495-afk <>
2026-06-23 11:49:07 +0000

    Boolean indexing

--- section/boolean_indexing/main.py
+++ section/boolean_indexing/main.py
@@ -1,5 +1,5 @@
 import numpy as np
-# Ratings of various products
-product_ratings = np.array([4, 2, 5, 3, 7, 1])
-# Retrieve the correct elements of product_ratings
-print(product_ratings[(product_ratings >= 3) & (product_ratings != 5)])
+# Temperatures recorded in two cities over four days
+city_temperatures = np.array([[12, 18, 25, 14], [32, 20, 28, 35]])
+# Retrieve the correct elements of city_temperatures
+print(city_temperatures[(city_temperatures <= 15) | (city_temperatures > 30)])
